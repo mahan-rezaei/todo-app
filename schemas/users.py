99b655b22b79a85a9fe80eb93c7553a4aa28f4f0
mahan-rezaei <mahanrezaei859@gmail.com>
2026-08-02 12:58:26 +0330

    add schemas for users./

---
+++ schemas/users.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, EmailStr
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    full_name: str
+    email: EmailStr
+    age: int | None = None
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserRead(UserBase):
+    id: int
+    created_at: datetime
+    updated_at: datetime
