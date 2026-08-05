954e46067d86134203fae43c17b3fb2da45e59b3
mahan-rezaei <mahanrezaei859@gmail.com>
2026-08-05 20:32:47 +0330

    add otp model./

--- models/users.py
+++ models/users.py
@@ -1,5 +1,7 @@
 from sqlmodel import SQLModel, Field
-from datetime import datetime, UTC
+from datetime import datetime, UTC, timedelta
+import string
+import secrets
 
 
 class User(SQLModel, table=True):
@@ -11,3 +13,22 @@ class User(SQLModel, table=True):
 
     created_at: datetime = Field(default_factory=datetime.now)
     updated_at: datetime = Field(default_factory=datetime.now)
+
+
+class OTP(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    email: str
+    code: str
+    expires_at: datetime
+    is_used: bool = False
+
+    @classmethod
+    def generate(cls, email: str, length=4):
+        digits = string.digits
+        code = ''.join(secrets.choice(digits) for _ in range(length))
+
+        return cls(email=email, code=code, expires_at=datetime.now() + timedelta(minutes=5))
+
+    def is_expired(self):
+        return datetime.now() > self.expires_at
+
