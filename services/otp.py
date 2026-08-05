9bec01b5c5ca802437625696b2830e4879844a0e
mahan-rezaei <mahanrezaei859@gmail.com>
2026-08-05 20:50:08 +0330

    add otp verify method./

--- services/otp.py
+++ services/otp.py
@@ -1,10 +1,10 @@
 from models.users import User, OTP
 from dependecies import SessionDep
-from sqlmodel import select
+from sqlmodel import select, desc
 
 
 async def verify_otp(email, code, session: SessionDep):
-    otp_instance = await session.exec(select(OTP).where(OTP.email==email, OTP.code==code))
+    otp_instance = await session.exec(select(OTP).where(OTP.email==email, OTP.code==code, OTP.is_used==False).order_by(desc(OTP.created_at)))
     otp_instance = otp_instance.first()
     if not otp_instance:
         return False
