2b7562e902619fcc37195a905ede2393e3f75267
mahan-rezaei <mahanrezaei859@gmail.com>
2026-08-13 18:55:34 +0330

    add update_task endpoint./

--- api/tasks.py
+++ api/tasks.py
@@ -28,3 +28,21 @@ async def create_task(task_data: TaskCreate, session: SessionDep, toekn=Depends(
     await session.commit()
     await session.refresh(task)
     return task
+
+
+@router.put('/update/{task_id}', status_code=status.HTTP_200_OK, response_model=TaskRead)
+async def update_task(task_data: TaskCreate, task_id, session: SessionDep, token=Depends(JWTBearer())):
+    t = decode_jwt(token.credentials)
+    user_instance = await session.exec(select(User).where(User.id==t['identifier']['id']))
+    user_instance = user_instance.first()
+    task_instance = await session.get(Task, task_id)
+
+    if task_instance.user_id != user_instance.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="you are not owner of this task.")
+    task_update_data = Task.model_dump(task_data, exclude_unset=True)
+    task_instance.sqlmodel_update(task_update_data)
+
+    session.add(task_instance)
+    await session.commit()
+    await session.refresh(task_instance)
+    return task_instance
